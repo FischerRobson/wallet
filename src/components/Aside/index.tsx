8c0340a4f2c47626e1c4918edd3ea5f405824f6f
Fischer <fischerrobson@gmail.com>
2021-05-24 21:15:24 -0300

    component aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,47 @@
 import React from "react";
-import { Container } from "./styles";
+import {
+  Container,
+  Header,
+  LogoImg,
+  Title,
+  MenuContainer,
+  MenuItemLink,
+} from "./styles";
+import logoImg from "../../assets/logo.svg";
+import {
+  MdDashboard,
+  MdArrowUpward,
+  MdArrowDownward,
+  MdExitToApp,
+} from "react-icons/md";
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogoImg src={logoImg} alt="Minha Carteira Logo" />
+        <Title>Minha Carteira</Title>
+      </Header>
+
+      <MenuContainer>
+        <MenuItemLink>
+          {" "}
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+        <MenuItemLink>
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+        <MenuItemLink>
+          <MdArrowDownward />
+          Saídas
+        </MenuItemLink>
+        <MenuItemLink>
+          <MdExitToApp />
+          Log Out
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 };
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -2,6 +2,52 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   grid-area: Aside;
-  color: ${props => props.theme.colors.white};
-  background: ${props => props.theme.colors.secondary};;
+  color: ${(props) => props.theme.colors.white};
+  background: ${(props) => props.theme.colors.secondary};
+  padding-left: 20px;
+
+  border-right: 1px solid ${(props) => props.theme.colors.gray};
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  height: 70px;
+`;
+
+export const LogoImg = styled.img`
+  height: 40px;
+  height: 40px;
+`;
+
+export const Title = styled.h3`
+  color: ${(props) => props.theme.colors.white};
+  margin-left: 10px;
+`;
+
+export const MenuContainer = styled.nav`
+  display: flex;
+  margin-top: 50px;
+  flex-direction: column;
+`;
+
+export const MenuItemLink = styled.a`
+  color: ${(props) => props.theme.colors.info};
+  text-decoration: none;
+  transition: opacity 1s;
+
+  display: flex;
+  align-items: center; 
+
+  margin: 7px 0;
+
+  &:hover{
+    opacity: 0.7;
+    cursor: pointer;
+  }
+
+  > svg {
+    font-size: 18px;
+    margin-right: 5px;
+  }
 `;
