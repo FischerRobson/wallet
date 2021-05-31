28de009dae11edbaaf1ad84242d8fe9441655153
Fischer <fischerrobson@gmail.com>
2021-05-31 20:50:29 -0300

    botoes de filtro na List

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -2,93 +2,34 @@ import React from "react";
 import ContentHeader from "../../components/ContentHeader";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 import SelectInput from "../../components/SelectInput";
-import { Container, Content } from "./styles";
+import { Container, Content, Filters } from "./styles";
 
 const List: React.FC = () => {
-  const options = [
-    { value: "a", label: "a" },
-    { value: "b", label: "b" },
+  const months = [
+    { value: 7, label: "Julho" },
+    { value: 8, label: "Agosto" },
+    { value: 9, label: "Setembro" },
+  ];
+
+  const years = [
+    { value: 2019, label: 2019 },
+    { value: 2020, label: 2020 },
+    { value: 2021, label: 2021 },
   ];
 
   return (
     <Container>
       <ContentHeader title="Entradas" lineColor="#fff">
-        <SelectInput options={options} />
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-recurrent">Recorrentes</button>
+        <button type="button" className="tag-filter tag-filter-eventual">Eventuais</button>
+      </Filters>
+
       <Content>
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />{" "}
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />{" "}
-        <HistoryFinanceCard
-          tagColor="#fff"
-          title="Conta"
-          subtitle="29/05/2021"
-          amount="250"
-        />{" "}
         <HistoryFinanceCard
           tagColor="#fff"
           title="Conta"
