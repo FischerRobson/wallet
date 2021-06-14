e7c1b2377ffc45b068482f412b84b2800306e104
Fischer <fischerrobson@gmail.com>
2021-06-13 22:17:24 -0300

    identacao do aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -15,7 +15,7 @@ import {
   MdExitToApp,
 } from "react-icons/md";
 import { useHistory } from "react-router-dom";
-import {FaWallet} from 'react-icons/fa'
+import { FaWallet } from "react-icons/fa";
 
 const Aside: React.FC = () => {
   const history = useHistory();
@@ -24,7 +24,7 @@ const Aside: React.FC = () => {
     <Container>
       <Header>
         {/* <LogoImg src={logoImg} alt="Minha Carteira Logo" /> */}
-        <FaWallet size={24} style={{marginRight: "15px"}}/>
+        <FaWallet size={24} style={{ marginRight: "15px" }} />
         <Title>Minha Carteira</Title>
       </Header>
 
